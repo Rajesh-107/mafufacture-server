8e384f6348cf49755cf1dcb9b6caa7da679f2482
Rajesh-107 <applerb326@gmail.com>
2022-09-06 01:51:06 +0600

    make admin api

--- index.js
+++ index.js
@@ -7,14 +7,53 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
+
 app.use(cors());
 app.use(express.json());
+
+// const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001',
+
+// const options = {
+//   origin: allowedOrigins
+// };
+
+// app.use(cors(options));
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1u7kw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })  
+  }
 
+
+//  function verifyJwt(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     if(!authHeader){
+//         return res.status(401).send({message: 'UnAuthorized access'});
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+//         if(err){
+//             return res.status(403).send({message: 'Forbidden access'})
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+//  } 
 
 async function run() {
     try{
@@ -53,10 +92,11 @@ async function run() {
       
         app.get('/order', async(req, res) => {
             const email = req.query.email;
-            
+            ;
             const query = {email: email};
             const Myorders = await orderCollection.find(query).toArray();
-            res.send(Myorders);
+            return res.send(Myorders);
+           
         })
         
         app.delete('/order/:id', async (req, res) => {
